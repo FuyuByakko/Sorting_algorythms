804ec0688dee602c54b57df1ddb5d946c9f874ae
MichailovIgor / MICHAILOV <igor.m.byak@gmail.com>
2019-01-16 12:40:09 +0900

    First full implementation of quick sort

--- quick_sort.rb
+++ quick_sort.rb
@@ -1,7 +1,13 @@
 def partition(arr, arr_start, arr_end)
+  #check if array is empty or of length 1. if Yes, no partitioning or sorting required
   if arr.length < 2
     return
   end
+  #if end of array is the same as start - 1 element array already sorted, otherwise no values to check
+  if arr_end <= arr_start
+    return
+  end
+
   #set index where the pivot will be shifted (all elements to the left of index are less than pivot)
   index_pos = arr_start
   #select a pivot point - in our case the element in the middle of the array
@@ -31,28 +37,60 @@ def partition(arr, arr_start, arr_end)
   #if the index is to the LEFT of pivot, pivot can be swapped with the element at the index itself
   if index_pos <= pivot
     arr[pivot], arr[index_pos] = arr[index_pos], arr[pivot]
-    puts "returning #{index_pos}"
-    puts "#{arr}"
+    #puts "returning #{index_pos}"
+    #puts "#{arr[arr_start..arr_end]}"
     return index_pos
   else
     arr[pivot], arr[index_pos - 1] = arr[index_pos - 1], arr[pivot]
-    puts "returning #{index_pos - 1}"
-    puts "#{arr}"
+    #puts "returning #{index_pos - 1}"
+    #puts "#{arr[arr_start..arr_end]}"
     return index_pos - 1
   end
 end
 
+#function received an array, default values for first iteration is the full array
+def quick_sort(arr, arr_start = 0, arr_end = arr.length - 1)
+  #if end of array is the same as start - 1 element array already sorted, otherwise no values to check
+  if arr_end <= arr_start
+    return
+  end
 
-partition([1,6,5,4,2], 0, 4)
+  #partition the received array and receive the pivot point index to divide it further
+  pivot = partition(arr, arr_start, arr_end)
+  #sort the remaining halves to the left and right of pivot
+  quick_sort(arr, arr_start, pivot - 1)
+  quick_sort(arr, pivot + 1, arr_end)
+  return
+end
 
 
+#checking partition function
+  #partition([1,6,5,4,2], 0, 4)
 
+#checking quick_sort
+def sorting_checks()
+  puts "sorting empty array []"
+  s1 = []
+  quick_sort([])
+  puts "#{s1}"
 
+  puts "sorting 1 element array [3]"
+  s2 = [3]
+  quick_sort(s2)
+  puts "#{s2}"
 
-#def quick_sort(arr, arr_start = 0, arr_end = arr.length - 1)
+  puts "sorting already sorted array [1,2,3,4,5]"
+  s3 = [1,2,3,4,5]
+  quick_sort(s3)
+  puts "#{s3}"
 
+  puts "sorting array already in descending order [3,1,-2]"
+  s4 = [3,1,-2]
+  quick_sort(s4)
+  puts "#{s4}"
 
-
-
-
-#end
+  puts "sorting random array [5,2,1,6,4,2,5,2,8,1,3,7]"
+  s5 = [5,2,1,6,4,2,5,2,8,1,3,7]
+  quick_sort(s5)
+  puts "#{s5}"
+end
