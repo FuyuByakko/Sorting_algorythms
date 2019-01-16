148e89ea6bc61ed5c8b6d89910c00a8c00efd28d
MichailovIgor / MICHAILOV <igor.m.byak@gmail.com>
2019-01-16 15:45:02 +0900

    Changed partitioning to move from both sides. Accept ANY position of pivot

--- quick_sort.rb
+++ quick_sort.rb
@@ -8,45 +8,61 @@ def partition(arr, arr_start, arr_end)
     return
   end
 
-  #want to change how the partitioning works
-
-  #set index where the pivot will be shifted (all elements to the left of index are less than pivot)
-  index_pos = arr_start
-  #select a pivot point - in our case the element in the middle of the array
+  #select a pivot point - in our case the element in the middle of the array as base
   pivot = (arr_end - arr_start) / 2 + arr_start
-  #iterate over the array and swap the currently checked element with the index_pos element if < pivot
-  for i in arr_start..arr_end
-    #need to skip the pivot item in the comparison
-    if i == pivot
-      next
-    end
-    #need to skip the the possible change of the pivot item
-    if index_pos == pivot
-      index_pos += 1
-    end
-    #compare the current item value with the pivot value
-    if arr[i] <= arr[pivot]
-      #if i is the same as index pos, all values before are less than pivot and no swapping was done
-      if i != index_pos
-        arr[i], arr[index_pos] = arr[index_pos], arr[i]
+  #for test can check any of the below as well
+  #pivot = arr_start
+  #pivot = arr_end
+
+  i = arr_start
+  j = arr_end
+  finish = false
+
+  #start counting from both sides
+  while !finish do
+    #iterate from left to right till we find a value that is larger than our pivot
+    while arr[i] <= arr[pivot] do
+      if i == j
+        finish = true
+        break
       end
-      index_pos += 1
+      i += 1
+    end
+    #iterate from right to left till we find a value that needs to be switched
+    while arr[j] >= arr[pivot] do
+      if i == j
+        finish = true
+        break
+      end
+      j -= 1
+    end
+    #swap the elements and increment
+    arr[i], arr[j] = arr[j], arr[i]
+    if !finish
+      i += 1
+      j -= 1
     end
   end
-  
-  #after all array is checked, the index is set right AFTER the last value less than pivot
-  #if index is to the RIGHT of pivot, we need to swap pivot with the previous element (pivot - 1)
-  #if the index is to the LEFT of pivot, pivot can be swapped with the element at the index itself
-  if index_pos <= pivot
-    arr[pivot], arr[index_pos] = arr[index_pos], arr[pivot]
-    #puts "returning #{index_pos}"
-    #puts "#{arr[arr_start..arr_end]}"
-    return index_pos
-  else
-    arr[pivot], arr[index_pos - 1] = arr[index_pos - 1], arr[pivot]
-    #puts "returning #{index_pos - 1}"
-    #puts "#{arr[arr_start..arr_end]}"
-    return index_pos - 1
+
+  #after we go through the whole array and our i and j meet, we check the meeting point
+  #if meeting point is located at the same index as pivot, no actions needed
+
+  #if meeting point is LEFT of pivot and is larger, we need to swap pivot with last element
+  if i < pivot && arr[i] >= arr[pivot]
+    arr[i], arr[pivot] = arr[pivot], arr[i]
+    return i
+  #if meeting point is LEFT of pivot and is smaller, we need to swap pivot with NEXT element    
+  elsif i < pivot && arr[i] < arr[pivot] 
+    arr[i + 1], arr[pivot] = arr[pivot], arr[i + 1]
+    return i + 1
+  #if meeting point is RIGHT of pivot and is larger, we need to swap pivot with PREVIOUS element
+  elsif i > pivot && arr[i] > arr[pivot]
+    arr[i - 1], arr[pivot] = arr[pivot], arr[i - 1]
+    return i - 1
+  #if meeting point is LEFT of pivot and is smaller, we need to swap pivot with NEXT element    
+  else 
+    arr[i], arr[pivot] = arr[pivot], arr[i]
+    return i        
   end
 end
 
@@ -67,32 +83,35 @@ end
 
 
 #checking partition function
-  #partition([1,6,5,4,2], 0, 4)
+# puts "partitioning: [1,6,5,4,2]"
+# partition([1,6,5,4,2], 0, 4)
 
-#checking quick_sort
-def sorting_checks()
-  puts "sorting empty array []"
-  s1 = []
-  quick_sort([])
-  puts "#{s1}"
+# checking quick_sort
+# def sorting_checks()
+#   puts "sorting empty array []"
+#   s1 = []
+#   quick_sort([])
+#   puts "#{s1}"
 
-  puts "sorting 1 element array [3]"
-  s2 = [3]
-  quick_sort(s2)
-  puts "#{s2}"
+#   puts "sorting 1 element array [3]"
+#   s2 = [3]
+#   quick_sort(s2)
+#   puts "#{s2}"
 
-  puts "sorting already sorted array [1,2,3,4,5]"
-  s3 = [1,2,3,4,5]
-  quick_sort(s3)
-  puts "#{s3}"
+#   puts "sorting already sorted array [1,2,3,4,5]"
+#   s3 = [1,2,3,4,5]
+#   quick_sort(s3)
+#   puts "#{s3}"
 
-  puts "sorting array already in descending order [3,1,-2]"
-  s4 = [3,1,-2]
-  quick_sort(s4)
-  puts "#{s4}"
+#   puts "sorting array already in descending order [3,1,-2]"
+#   s4 = [3,1,-2]
+#   quick_sort(s4)
+#   puts "#{s4}"
 
-  puts "sorting random array [5,2,1,6,4,2,5,2,8,1,3,7]"
-  s5 = [5,2,1,6,4,2,5,2,8,1,3,7]
-  quick_sort(s5)
-  puts "#{s5}"
-end
+#   puts "sorting random array [5,2,1,6,4,2,5,2,8,1,3,7]"
+#   s5 = [5,2,1,6,4,2,5,2,8,1,3,7]
+#   quick_sort(s5)
+#   puts "#{s5}"
+# end
+
+# sorting_checks()
